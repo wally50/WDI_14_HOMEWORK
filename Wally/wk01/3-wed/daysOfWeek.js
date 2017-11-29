5b85de5c7d9057cada7acffcabf586b89e16cdb7
Wally <wvdberg@gmail.com>
2017-11-30 00:30:16 +1100

    days of the week

---
+++ Wally/wk01/3-wed/daysOfWeek.js
@@ -0,0 +1,22 @@
+console.log('ready');
+
+var days_of_the_week = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+console.log(days_of_the_week);
+
+var tempDay = days_of_the_week.pop();
+console.log(days_of_the_week);
+
+var newDoW = days_of_the_week.unshift(tempDay);
+console.log(days_of_the_week);
+
+var weekDays = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"];
+var weekendDays = ["Saturday", "Sunday"];
+var week = new Array (weekDays, weekendDays);
+//console.log(weekDays + "," + weekendDays);
+console.log(week);
+
+var weekDays2 = week.pop();
+console.log(week);
+
+var alphaDays = week.sort();
+console.log(alphaDays);
