5122e73c56aa6d817bf624446802fcd3d7dde12b
Wally <wvdberg@gmail.com>
2017-12-10 22:45:11 +1100

    pretty much finishe

--- Wally/wk02/bank_wireframe/bank.js
+++ Wally/wk02/bank_wireframe/bank.js
@@ -1,4 +1,105 @@
 console.log('Bank - ready');
 
-var withdrawButton = document.querySelector('.wdraw');
-var depositButton = document.querySelector('.deposit');
+var savingsBalance = 0.00;
+var savBalValue = "0.00";
+//var transAmount = 0;
+var chequeBalance = 0;
+var chqBalValue = "0.00";
+
+var enoughFundsTest ='True';
+
+var savBalValue = document.querySelector('.savBalValue');
+var savTransAmount=document.querySelector('.savTransAmount');
+var savWithdrawButton = document.querySelector('.savWithdraw');
+var savDepositButton = document.querySelector('.savDeposit');
+var savTransferButton = document.querySelector('.savTransfer');
+
+var chqBalValue = document.querySelector('.chqBalValue');
+var chqTransAmount=document.querySelector('.chqTransAmount');
+var chqWithdrawButton = document.querySelector('.chqWithdraw');
+var chqDepositButton = document.querySelector('.chqDeposit');
+var chqTransferButton = document.querySelector('.chqTransfer');
+
+
+var savIsThereEnoughFunds = function (){
+  var testAmt = ( Number(savTransAmount.value));
+  var testBal = (savingsBalance);
+  if (Number(savTransAmount.value) <= savingsBalance || (Math.abs(testAmt - testBal) < 0.0001) ) {
+    savWithdraw();
+  }  else {
+    enoughFundsTest = 'False';
+    alert("Not Enough Funds - Transfer Funds or reduce withdrawal amount");
+  }
+};
+
+var chqIsThereEnoughFunds = function (){
+  var testAmt = ( Number(chqTransAmount.value));
+  var testBal = (chequeBalance);
+  if (Number(chqTransAmount.value) <= chequeBalance || (Math.abs(testAmt - testBal) < 0.0001) ) {
+    chqWithdraw();
+  }  else {
+    enoughFundsTest = 'False';
+    alert("Not Enough Funds - Transfer Funds or reduce withdrawal amount");
+  }
+};
+
+function savDeposit(){
+  savingsBalance = savingsBalance + Number(savTransAmount.value);
+  savBalValue.textContent = savingsBalance.toFixed (2);
+  console.log('savingsBalance ' + savingsBalance);
+  console.log(savBalValue);
+}
+
+function savWithdraw(){
+  savingsBalance = savingsBalance - Number(savTransAmount.value);
+  savingsBalance = Math.abs(savingsBalance);
+  savBalValue.textContent = savingsBalance.toFixed (2);
+  console.log('savingsBalance ' + savingsBalance);
+}
+
+function savTransfer () {
+  enoughFundsTest = 'True';
+  savIsThereEnoughFunds ();
+  chqTransAmount = savTransAmount;
+    if (enoughFundsTest === 'True') {
+      chqDeposit();
+    }
+        console.log(chqTransAmount);
+  chqTransAmount=document.querySelector('.chqTransAmount');
+  enoughFundsTest = 'True';
+}
+
+function chqDeposit () {
+  chequeBalance = chequeBalance + Number(chqTransAmount.value);
+  chqBalValue.textContent = chequeBalance.toFixed (2);
+  console.log('chequeBalance ' + chequeBalance);
+  console.log(chqBalValue);
+}
+
+function chqWithdraw () {
+  chequeBalance = chequeBalance - Number(chqTransAmount.value);
+  chequeBalance = Math.abs(chequeBalance);
+  chqBalValue.textContent = chequeBalance.toFixed (2);
+  console.log('chequeBalance ' + chequeBalance);
+}
+
+function chqTransfer () {
+  enoughFundsTest = 'True';
+  chqIsThereEnoughFunds ();
+  savTransAmount = chqTransAmount;
+  if (enoughFundsTest === 'True') {
+  savDeposit();
+}
+  console.log(savTransAmount);
+  savTransAmount=document.querySelector('.savTransAmount');
+  enoughFundsTest = 'True';
+}
+
+
+savDepositButton.addEventListener('click', savDeposit);
+savWithdrawButton.addEventListener('click', savIsThereEnoughFunds);
+savTransferButton.addEventListener('click', savTransfer);
+
+chqDepositButton.addEventListener('click', chqDeposit);
+chqWithdrawButton.addEventListener('click', chqIsThereEnoughFunds);
+chqTransferButton.addEventListener('click', chqTransfer);
