ac9bd43e9cbb1bf1cd9002e68333537b54ea60df
Wally <wvdberg@gmail.com>
2018-02-20 09:12:29 +1100

    es6 classes atm

---
+++ Wally/wk11/1-mon/es6_classes_atm/atm.js
@@ -0,0 +1,40 @@
+
+
+class ATM {
+  constructor (type, money, transactionHistory) {
+    this.type = type;
+    this.money = money;
+    this.transactionHistory = transactionHistory;
+  }
+
+
+  transactionHistory() {
+    console.log(this.transactionHistory);
+    return this.transactionHistory;
+  }
+
+  get showBalance() {
+    console.log(this.money);
+    return this.money;
+  }
+
+  withdraw(amount) {
+    this.transactionHistory.push(-amount);
+    this.money = this.money - amount;
+    return this.money;
+  }
+
+  deposit(amount) {
+    this.transactionHistory.push(amount);
+    this.money = this.money + amount;
+    return this.money;
+  }
+
+}
+
+let savings = new ATM ("savings", 0, [0]);
+
+savings.deposit(100);
+savings.withdraw(30);
+savings.showBalance;
+savings.transactionHistory;
---
+++ Wally/wk11/1-mon/es6_classes_atm/atm.md
@@ -0,0 +1,15 @@
+Make an ATM Class
+For this exercise you will be creating an ATM class.
+
+It will have the following properties...
+
+type (e.g., "checking"), which should be determined by some input
+money, which should start out as 0
+It should have the following methods...
+
+withdraw, which should decrease the amount of money by some input
+deposit, which should increase the amount of money by some input
+showBalance, which should print the amount of money in the bank to the console.
+The Atm class has a transactionHistory property which keeps track of the withdrawals and deposits made to the account.
+
+Make sure to indicate whether the transaction increased or decreased the amount of money in the bank.
